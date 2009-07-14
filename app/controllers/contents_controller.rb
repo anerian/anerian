9f107dfeb2b716f2324cace8d1dcce50b264abce
Todd Fisher <taf2@glazgo.(none)>
2009-07-14 16:59:48 -0400

    updating with some more forms

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,26 +1,44 @@
 class ContentsController < ApplicationController
   def index
+    @contents = Content.paginate(:all, :page => params[:page])
   end
 
   def show
+    @content = Content.find_by_id(params[:id])
   end
 
   def new
+    @content = Content.new
   end
 
   def create
+    @content = Content.new(params[:content])
+    if @content.save
+      redirect_to contents_path
+    else
+      render :action => 'new'
+    end
   end
 
   def edit
+    @content = Content.find_by_id(params[:id])
   end
 
   def update
+    @content = Content.find_by_id(params[:id])
+    if @content.update_attributes(params[:content])
+      redirect_to contents_path
+    else
+      render :action => 'edit'
+    end
   end
 
   def delete
+    @content = Content.find_by_id(params[:id])
   end
 
   def destroy
+    @content = Content.find_by_id(params[:id])
   end
 
 end
--- site/lib/helpers.rb
+++ site/lib/helpers.rb
@@ -7,4 +7,25 @@ helpers do
     erb.result(binding)
   end
 
+  def video(id, video, width, height)
+    #flashvars = 'config={"plugins": {"pseudo": {"url": "http://anerian.ws/flash/flowplayer/flowplayer.pseudostreaming-3.1.2.swf"}, "controls": {"backgroundColor": "transparent", "backgroundGradient": "none", "buttonColor": "transparent", "all": false } }, "clip": {"url": "http://anerian.ws/files/test.flv", "autoPlay": true, "provider": "pseudo"}}';
+    video_base   = 'http://anerian.ws/files/MDEyY2NlNDIzYWQ2/51YKYf3oQVH1'
+    config = ActiveSupport::JSON.encode({
+      "plugins" => {
+        "pseudo" => {
+          "url" => "http://anerian.ws/flash/flowplayer/flowplayer.pseudostreaming-3.1.2.swf"
+        }
+      },
+      "clip" => {
+        "autoPlay" => false,
+        "provider" => "pseudo",
+        "url" => "#{ video_base }.mp4"
+      }
+    })
+    flash_config = %(config=#{config})
+    flash_player = "http://anerian.ws/flash/flowplayer/flowplayer-3.1.0.swf"
+    erb = ERB.new(File.read("#{AppRoot}/lib/partials/_video.erb"))
+    erb.result(binding)
+  end
+
 end
